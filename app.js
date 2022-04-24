3895afc4e5a177b6e66e42419733375631121849
mariamee <mariameelalaoui@gmail.com>
2022-04-24 17:40:02 +0000

    Add command line application to get a summary description to the subject from wikepedia

---
+++ app.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+const prompt = require("prompt-sync")({ sigint: true });
+
+const getSummary = async () => {
+  const title = prompt(" What is the subject you want to summarize ? ");
+  const subject = title.replace(/\s/g, "+");
+  const { data } = await axios.get(
+    `https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=&explaintext=&titles=${subject}&format=json`
+  );
+  const id = Object.keys(data.query.pages)[0];
+  console.log(data.query.pages[id].extract);
+};
+const main = async () => {
+  await getSummary();
+  while (true) {
+    const answer = prompt("Do you want to continue ? (y/n)");
+    if (answer === "y") {
+      await getSummary();
+    } else {
+      break;
+    }
+  }
+};
+main();
